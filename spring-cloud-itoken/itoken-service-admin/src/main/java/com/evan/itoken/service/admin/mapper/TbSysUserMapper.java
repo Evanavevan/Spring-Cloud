590496cb58c61117b465021e23c96bc73cd06def
evan <44725772+Evanavevan@users.noreply.github.com>
2021-10-26 20:23:54 +0800

    Add files via upload

---
+++ spring-cloud-itoken/itoken-service-admin/src/main/java/com/evan/itoken/service/admin/mapper/TbSysUserMapper.java
@@ -0,0 +1,7 @@
+package com.evan.itoken.service.admin.mapper;
+
+import com.evan.itoken.service.admin.domain.TbSysUser;
+import tk.mybatis.MyMapper;
+
+public interface TbSysUserMapper extends MyMapper<TbSysUser> {
+}
---
+++ spring-cloud-itoken/itoken-service-admin/src/main/java/com/evan/itoken/service/admin/service/AdminService.java
@@ -0,0 +1,19 @@
+package com.evan.itoken.service.admin.service;
+
+import com.evan.itoken.service.admin.domain.TbSysUser;
+
+public interface AdminService {
+    /**
+     * 注册
+     * @param tbSysUser {@link TbSysUser}
+     */
+    void register(TbSysUser tbSysUser);
+
+    /**
+     * 登录
+     * @param loginCode 登录账号
+     * @param plantPassword 明文登录密码
+     * @return
+     */
+    TbSysUser login(String loginCode, String plantPassword);
+}
